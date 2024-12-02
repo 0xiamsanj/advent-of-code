3046f23b2be036480ff34ca698a468382ad34c9f
0xiamsanj <sanjai04022002@gmail.com>
2024-12-02 23:38:11 +0530

    C++ template updated

--- templates/template_cpp.cpp
+++ templates/template_cpp.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 #include <fstream>
 #include <string>
-using namespace std;
-
 
 
 // Read input from the input files
-
 std::vector<std::string> read_input(const std::string filename){
+    std::fstream file_stream(filename);
+    std::vector<std::string> input;
 
+
+    return input;
 }
+
 // Part-1 code to be executed here
 void solve_part1(const std::vector<std::string>& input){
     std::cout << "--------- Part-1 Solution --------- " << std::endl;
@@ -21,9 +23,10 @@ void solve_part2(const std::vector<std::string>& input){
     std::cout << "--------- Part-2 Solution --------- " << std::endl;
 }
 int main(int argc, char **argv){
-    std::string input_filename_1 = "input_1.txt";
-    std::string input_filename_2 = "input_2.txt";
+    std::string input_file = "input.txt";
+
+    std::vector<std::string> input = read_input(input_file);
 
     // Solving part-1
-    std::string input_string;
+    solve_part1(input);
 }
